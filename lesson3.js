7bc70466a2090394b52144a3a0edc6d05dde72bc
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-07-26 00:58:43 +0530

    lesson3

--- lesson3.js
+++ lesson3.js
@@ -33,8 +33,36 @@ const companies = [
 
 const ages = [32, 12, 20, 16, 5, 54, 21, 44, 61, 12, 15, 45, 25, 64, 32];
 
+console.log(ages.sort());
+
 //  for each
 
-// companies.forEach((company) => {
-//   console.log(company);
-// });
+companies.forEach((company) => {
+  console.log(company);
+});
+
+//filter
+
+console.log(ages.filter((age) => age < 30));
+
+const productCompany = companies.filter(
+  (company) => company.category === "Product Based"
+);
+
+console.log(productCompany);
+
+//map
+
+const stringLiteral = companies.map((comapny, index) => {
+  return `name:${comapny.name} ${comapny.category}`;
+});
+
+console.log(stringLiteral);
+
+// sort
+
+const sortedStart = companies.sort((c1, c2) => (c1.start > c2.start ? 1 : -1));
+console.log(sortedStart);
+
+const sortedName = companies.sort((c1, c2) => c1.name.localeCompare(c2.name));
+console.log(sortedName);
