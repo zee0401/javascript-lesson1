c3e85441f074a408526177579eeace4daccfaf99
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-07-25 15:54:39 +0530

    lesson 2 completed

--- lesson2.js
+++ lesson2.js
@@ -26,3 +26,50 @@ const users = [
     age: 30,
   },
 ];
+
+// get names using for
+
+const namesfor = [];
+
+for (i = 0; i < users.length; i++) {
+  names.push(users[i].name);
+}
+
+console.log(names);
+
+// get name using forEach
+
+const nameeach = [];
+
+users.forEach((user) => {
+  nameeach.push(user.name);
+});
+
+console.log(nameeach);
+
+// using map
+
+const userName = users.map((user) => {
+  return user.name;
+});
+
+console.log(userName);
+
+// filter isactive users
+
+const isActiveUser = users
+  .filter((user) => user.isActive)
+  .map((user) => user.name);
+console.log(isActiveUser);
+
+//sort
+
+const sorting = users.sort((user1, user2) => (user1.age < user2.age ? 1 : -1));
+console.log(sorting);
+
+const sorted = users
+  .filter((user) => user.isActive)
+  .sort((user1, user2) => (user1.age < user2.age ? 1 : -1))
+  .map((user) => user.name);
+
+console.log(sorted);
