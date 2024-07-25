356022a1978463f29356e892305039fe457d17e4
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-07-26 00:30:39 +0530

    lesson3 created

---
+++ lesson3.js
@@ -0,0 +1,40 @@
+const companies = [
+  {
+    name: "Google",
+    category: "Product Based",
+    start: 1981,
+    end: 2004,
+  },
+  {
+    name: "Amazon",
+    category: "Product Based",
+    start: 1992,
+    end: 2008,
+  },
+  {
+    name: "Paytm",
+    category: "Product Based",
+    start: 1999,
+    end: 2010,
+  },
+  {
+    name: "Coforge",
+    category: "Service Based",
+    start: 1989,
+    end: 2010,
+  },
+  {
+    name: "Mindtree",
+    category: "Service Based",
+    start: 1989,
+    end: 2010,
+  },
+];
+
+const ages = [32, 12, 20, 16, 5, 54, 21, 44, 61, 12, 15, 45, 25, 64, 32];
+
+//  for each
+
+// companies.forEach((company) => {
+//   console.log(company);
+// });
